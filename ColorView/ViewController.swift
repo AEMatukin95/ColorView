e187cfe1843de8fe1ad7ad332540a2beb38b071b
Aleksandr Matukin <aleksandr.matukin@icloud.com>
2021-10-24 01:44:49 +0300

    Project

--- ColorView/ViewController.swift
+++ ColorView/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
     }
 
+    func viewColors() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    @IBAction func redSliderAction(_ sender: Any) {
+        viewColors()
+        redLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        viewColors()
+        greenLabel.text = String(greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction(_ sender: Any) {
+        viewColors()
+        blueLabel.text = String(blueSlider.value)
+    }
+    
 
 }
 
